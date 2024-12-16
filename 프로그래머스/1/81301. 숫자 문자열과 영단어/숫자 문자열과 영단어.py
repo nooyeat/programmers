8a44073f0657e3ed59a996d1050bf2343ff3133c
이태윤 <dlxodbs0902@gmail.com>
2024-12-16 19:31:17 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = 0
+    a=s.replace('zero','0').\
+    replace('one','1').\
+    replace('two','2').\
+    replace('three','3').\
+    replace('four','4').\
+    replace('five','5').\
+    replace('six','6').\
+    replace('seven','7').\
+    replace('eight','8').\
+    replace('nine','9')
+    answer=int(a)
+    return answer
+    
