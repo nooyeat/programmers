2544fd6f996fb3bebddecd08b42f2e34220c2d1f
이태윤 <dlxodbs0902@gmail.com>
2025-02-24 13:39:24 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.26 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,23 @@
+import re
+def solution(new_id):
+    new_id = new_id.lower()
+    new_id = re.sub(r'[^a-z0-9-_.]', '', new_id)
+    new_id = re.sub(r'\.{2,}','.',new_id)
+
+    if new_id and new_id[0] == '.':
+        new_id = new_id[1:]
+    if new_id and new_id[-1] == '.':
+        new_id = new_id[:-1]
+
+    if new_id == '':
+        new_id = 'a'
+
+    if len(new_id) >= 16:
+        new_id = new_id[:15]
+        if new_id[-1] == '.':
+            new_id = new_id[:-1]
+
+    while len(new_id) < 3:
+        new_id += new_id[-1]
+
+    return new_id
