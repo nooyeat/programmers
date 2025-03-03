1a674ee2986092ae1c66ee190e33ea02c981a6a3
이태윤 <dlxodbs0902@gmail.com>
2025-03-03 16:41:07 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,5 +1,7 @@
 -- 코드를 입력하세요
 SELECT *
-from places
-where host_id in (select host_id from places group by host_id having count(host_id)>=2)
-order by id;
+FROM places
+WHERE host_id in (SELECT host_id
+                 FROM places
+                 GROUP BY 1
+                 HAVING count(host_id) >= 2)
