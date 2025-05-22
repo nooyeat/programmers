0e56408880e7b97c2300fee92d6cdb7e19b06109
이태윤 <dlxodbs0902@gmail.com>
2025-05-22 09:27:27 +0900

    Delete 프로그래머스/3/59044. 오랜 기간 보호한 동물（1） directory

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++
@@ -1,8 +0,0 @@
--- 코드를 입력하세요
-SELECT ai.name, ai.datetime
-FROM animal_ins ai
-LEFT JOIN animal_outs ao on ai.animal_id = ao.animal_id
-WHERE ai.animal_id not in (SELECT animal_id
-                          FROM animal_outs)
-ORDER BY ai.datetime
-LIMIT 3;
