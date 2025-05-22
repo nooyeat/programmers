6b0f3b6d836a31ea334b7ec106f75bc58c4bf531
이태윤 <dlxodbs0902@gmail.com>
2025-05-22 09:25:51 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,6 +1,8 @@
 -- 코드를 입력하세요
 SELECT ai.name, ai.datetime
-from animal_ins ai left join animal_outs ao on ai.animal_id = ao.animal_id
-where ao.animal_id is null
-order by ai.datetime
-limit 3;
+FROM animal_ins ai
+LEFT JOIN animal_outs ao on ai.animal_id = ao.animal_id
+WHERE ai.animal_id not in (SELECT animal_id
+                          FROM animal_outs)
+ORDER BY ai.datetime
+LIMIT 3;
